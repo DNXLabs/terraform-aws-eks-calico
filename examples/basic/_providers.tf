980e1b9bc89a5c59379d0df62b0e16192015f975
lucasmacedot <lucas.macedo@dnx.solutions>
2024-04-23 21:16:21 -0400

    Update versions on examples

--- examples/basic/_providers.tf
+++ examples/basic/_providers.tf
@@ -1,11 +1,3 @@
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
-}
-
 provider "aws" {
   region = var.region
 }
@@ -14,7 +6,6 @@ provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
-  load_config_file       = false
 }
 
 provider "helm" {
